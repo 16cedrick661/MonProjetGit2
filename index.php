13364600cc96ea5ec6efb863bde7b21baf186d4f
Cédrick COSTERG <cedrickcosterg@gmail.com>
2020-11-19 13:52:17 +0100

    version 1

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+
+
+    <title>Mon Projet GIT 2</title>
+</head>
+<body>
+
+
+<div class="container">
+
+
+    <h1 class="display-4 text-center my-4 shadow-lg">Mon Projet GIT 2 (ou pas)</h1>
+
+    <form method="post" action="" class="form-inline">
+    <div class="form-group mr-2">
+        <input type="text" class="form-control" id="search" name="search">
+    </div>
+    <input type="submit" id="submit" value="CHERCHONS" class="btn btn-dark mr-2">
+    <button type="button" id="reset" value="reset" class="btn btn-dark">Retour</button>
+</form>    
+
+
+    </div>
+
+</div>
+
+<script
+    src="http://code.jquery.com/jquery-3.5.1.min.js"
+  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+  crossorigin="anonymous"></script>
+
+    <script src="ajax5.js"></script>
+
+</body>
+</html>    
